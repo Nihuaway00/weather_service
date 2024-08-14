e926d2f99f43f9582dc1b8cac620a35790e34701
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-14 07:53:32 +0300

    Дописал добавление локации

--- src/main/java/entity/location/LocationController.java
+++ src/main/java/entity/location/LocationController.java
@@ -3,36 +3,23 @@ package entity.location;
 import Service.OpenWeatherService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import entity.user.User;
 import entity.user.UserDao;
-import entity.user.UserPayloadDto;
 import exceptions.BadRequestToServiceException;
-import exceptions.LocationControllerException;
 import exceptions.OpenWeatherServiceException;
-import exceptions.UserNotExists;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.apache.http.client.utils.URIBuilder;
-import org.hibernate.Session;
-import utils.HibernateUtil;
-import utils.HttpClientUtil;
 import utils.JwtUtil;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 @WebServlet(name = "locationController", value = "/location/*")
 public class LocationController extends HttpServlet {
@@ -76,8 +63,6 @@ public class LocationController extends HttpServlet {
             }
         } catch (JsonProcessingException e){
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Ошибка при парсинге JSON: " + e.getMessage());
-        } catch (ExpiredJwtException e){
-            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Токен истек. Вам нужна повторная авторизация: " + e.getMessage());
         } catch (OpenWeatherServiceException e){
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Проблема с внешним сервисом погоды. " + e.getMessage());
         } catch (BadRequestToServiceException e){
@@ -92,17 +77,9 @@ public class LocationController extends HttpServlet {
     }
 
     public void addLocation(HttpServletRequest req, HttpServletResponse resp)
-            throws BadRequestToServiceException, OpenWeatherServiceException, ExpiredJwtException, IOException {
+            throws BadRequestToServiceException, OpenWeatherServiceException, IOException {
         String result;
-        HttpSession session = req.getSession();
-        String token = session.getAttribute("token").toString();
-        Jwt<?, ?> jwt = JwtUtil.parseToken(token);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> payload = objectMapper.readValue(JwtUtil.parsePayloadToJson(jwt.getPayload().toString()), Map.class);
-
-        Long userId = Long.valueOf(payload.get("id").toString());
-
+        Long userId = Long.valueOf(req.getAttribute("userId").toString());
         String city = req.getReader().readLine();
 
         WeatherResponseDto weatherResponseDto = openWeatherService.getWeather(city);
